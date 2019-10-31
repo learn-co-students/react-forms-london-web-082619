3a1e268f5cdcbf595181505a58c1352014afd1b7
github username <github email address>
2019-10-31 17:54:02 +0000

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,8 @@ import React from 'react';
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    submittedData: []
   }
 
   handleFirstNameChange = event => {
@@ -18,11 +19,28 @@ class Form extends React.Component {
     })
   }
 
+  sendData = (dataObj) => {
+      this.setState({
+        submittedData: dataObj
+      })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Sent to submittedData')
+    let dataObject = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
+      }
+      this.sendData(dataObject)
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={(e) => this.handleSubmit(e)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+        <button type='submit'>submit</button>
       </form>
     )
   }
